cad2455d7f4a907397f94e80511db738cf2585d0
Jay Schwane <jrschwane@uwalumni.com>
2016-05-24 21:39:20 -0500

    update db url

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // config/db.js
 
 module.exports = {
-  url : 'mongodb://albumsuser:bumsus!.@ds051605.mongolab.com:51605/node-albums'
+  url : 'mongodb://albumsuser:bumsus!@ds051605.mlab.com:51605/node-albums'
 }
